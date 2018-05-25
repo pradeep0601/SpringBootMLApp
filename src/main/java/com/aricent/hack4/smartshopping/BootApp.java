87b7597377a5638b349e1038d536bf66cb932be0
Pradeep Kumar <pradeepk.pdp@gmail.com>
2018-05-26 01:31:59 +0530

    first commit

---
+++ src/main/java/com/aricent/hack4/smartshopping/BootApp.java
@@ -0,0 +1,13 @@
+package com.aricent.hack4.smartshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration 
+public class BootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApp.class, args);
+	}
+
+}
